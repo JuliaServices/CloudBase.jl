b9f94b490175dd1d7d05518d8921bcafdf245f90
Jacob Quinn <quinn.jacobd@gmail.com>
2022-09-12 12:37:18 -0600

    oops

--- src/CloudTest.jl
+++ src/CloudTest.jl
@@ -159,7 +159,7 @@ function run(; dir=nothing, bucket=nothing, public=false, startupDelay=0.25, deb
     bkt = AWS.Bucket(something(bucket, "jl-minio-$(abs(rand(Int16)))"); host="http://127.0.0.1:$port")
     headers = public ? ["X-Amz-Acl" => "public-read-write"] : []
     resp = AWS.put(bkt.baseurl, headers; service="s3", credentials, status_exception=false)
-    while resp != 200
+    while resp.status != 200
         if resp.status == 503
             # minio server is still starting up, so wait a bit and try again
             sleep(startupDelay)
